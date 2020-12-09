8a9e9b61758b6ccd535c749746e86c7f580d1f34
Wojtek <wojtek.kajstura@gmail.com>
2020-12-09 17:11:36 +0100

    added stats fragment

--- Bezpiecznik/app/src/main/java/com/example/bezpiecznik/view/StatsFragment.kt
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/view/StatsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.bezpiecznik
+package com.example.bezpiecznik.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.bezpiecznik.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
