982ef736e70a9ecfe216352cd996b542b35cb948
Wojtek <wojtek.kajstura@gmail.com>
2020-12-16 21:58:27 +0100

    implemented attributes of row and col in class

--- Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/PatternLockView.kt
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/PatternLockView.kt
@@ -6,10 +6,21 @@ import android.widget.GridLayout
 import com.example.bezpiecznik.R
 
 class PatternLockView :GridLayout{
-
+    var patternRowCount = 0
+    var patternColCount = 0
 
     constructor(context:Context) : super(context)
     constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet){
+        var attributes = context.obtainStyledAttributes(attributeSet, R.styleable.PatternLock)
+
+        // TODO: Think about getIntegerOrThrow
+        patternRowCount = attributes.getInteger(R.styleable.PatternLock_pattern_row_count, 3)
+        patternColCount = attributes.getInteger(R.styleable.PatternLock_pattern_col_count, 3)
+
+        attributes.recycle()
+
+        rowCount = patternRowCount
+        columnCount = patternColCount
 
     }
 }
