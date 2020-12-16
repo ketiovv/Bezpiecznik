f54c34f471e74c6beb7611b6935effc0f4331f09
Wojtek <wojtek.kajstura@gmail.com>
2020-12-16 21:56:03 +0100

    created child class of grid layout

---
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/PatternLockView.kt
@@ -0,0 +1,15 @@
+package com.example.bezpiecznik.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.GridLayout
+import com.example.bezpiecznik.R
+
+class PatternLockView :GridLayout{
+
+
+    constructor(context:Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet){
+
+    }
+}
