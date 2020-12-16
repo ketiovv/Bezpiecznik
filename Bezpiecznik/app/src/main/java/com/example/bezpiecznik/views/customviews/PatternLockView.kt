ba3396ba46f45fb4371822ba2990375a87114cd4
Wojtek <wojtek.kajstura@gmail.com>
2020-12-17 00:30:44 +0100

    implemented drawing dots

--- Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/customviews/PatternLockView.kt
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/customviews/PatternLockView.kt
@@ -1,11 +1,20 @@
-package com.example.bezpiecznik.views
+package com.example.bezpiecznik.views.customviews
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.View
 import android.widget.GridLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.setPadding
 import com.example.bezpiecznik.R
+import java.util.ArrayList
 
 class PatternLockView :GridLayout{
+
     var patternRowCount = 0
     var patternColCount = 0
 
@@ -22,5 +31,15 @@ class PatternLockView :GridLayout{
         rowCount = patternRowCount
         columnCount = patternColCount
 
+        setupDots()
+    }
+
+    fun setupDots(){
+        for(x in 0 until patternColCount){
+            for(y in 0 until patternRowCount){
+                var cell = CellView(context, patternColCount)
+                addView(cell)
+            }
+        }
     }
 }
