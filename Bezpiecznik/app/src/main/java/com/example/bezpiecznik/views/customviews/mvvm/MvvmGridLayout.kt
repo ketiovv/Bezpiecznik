4997bbc7c36186bbe6d30a6b2a698c400c38ef40
Wojtek <wojtek.kajstura@gmail.com>
2021-01-05 21:42:28 +0100

    created abstract class of grid layout mvvm implementation

---
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/customviews/mvvm/MvvmGridLayout.kt
@@ -0,0 +1,34 @@
+package com.example.bezpiecznik.views.customviews.mvvm
+
+import android.content.Context
+import android.os.Parcelable
+import android.util.AttributeSet
+import android.widget.GridLayout
+import androidx.lifecycle.LifecycleOwner
+import com.example.bezpiecznik.views.customviews.mvvm.exceptions.LifecycleOwnerNotFoundException
+import com.example.bezpiecznik.views.customviews.mvvm.interfaces.IMvvmCustomView
+import com.example.bezpiecznik.views.customviews.mvvm.interfaces.MvvmCustomViewModel
+import com.example.bezpiecznik.views.customviews.mvvm.interfaces.MvvmCustomViewState
+
+abstract class MvvmGridLayout<V: MvvmCustomViewState, T: MvvmCustomViewModel<V>>(
+        context: Context,
+        attributeSet: AttributeSet
+) :
+        GridLayout(context, attributeSet),
+        IMvvmCustomView<V, T> {
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val lifecycleOwner = context as? LifecycleOwner ?: throw LifecycleOwnerNotFoundException()
+        onLifecycleOwnerAttached(lifecycleOwner)
+    }
+
+    override fun onSaveInstanceState() = MvvmCustomViewStateWrapper(super.onSaveInstanceState(), viewModel.state)
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state is MvvmCustomViewStateWrapper) {
+            viewModel.state = state.state as V?
+            super.onRestoreInstanceState(state.superState)
+        }
+    }
+}
