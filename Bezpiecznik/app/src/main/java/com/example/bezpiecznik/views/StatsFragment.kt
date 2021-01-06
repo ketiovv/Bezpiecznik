10c931a56294cb4edd506b4ebeb84f6e89f16b74
Konrad <konrlub566@student.polsl.pl>
2021-01-06 15:32:30 +0100

    Accessing about fragment from settings fragment

--- Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/StatsFragment.kt
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/StatsFragment.kt
@@ -24,52 +24,52 @@ import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
 
 class StatsFragment : Fragment() {
-    lateinit var viewManager: RecyclerView.LayoutManager
-    lateinit var statsListAdapter: StatsListAdapter
+//    lateinit var viewManager: RecyclerView.LayoutManager
+//    lateinit var statsListAdapter: StatsListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//      TODO: Update the json get logic
 
+//        viewManager = LinearLayoutManager(requireContext())
+//
+//        // Inflate the layout for this fragment
+//        var userList = MutableLiveData<MutableList<User>>()
+//
+//
+//        var user = User("1","Konrad","Konrad123", mutableListOf(),
+//            Pattern(mutableListOf(Cell(1,0))),Settings())
+//
+//        var gson = Gson()
+//        var jsonString = gson.toJson(user)
+//        Log.d("myTag",jsonString)
+//
+//        val api = Retrofit.Builder().baseUrl(ApiRoutes.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(IApiRequest::class.java)
+//
+//        //api.addJSon(user)
+//
+//
+//        GlobalScope.launch(Dispatchers.IO) {
+//            val response = api.getJson().awaitResponse()
+//            if (response.isSuccessful){
+//                val data= response.body()!!
+//                // postValue instead of value, because it's called asynchronous
+//                userList.postValue(mutableListOf(data))
+//                Log.d("api-connection",userList.value!!.toString())
+//
+//            }
+//            else{
+//                Log.d("api-connection","response failed")
+//                userList.postValue(ArrayList())
+//            }
+//        }
 
-        viewManager = LinearLayoutManager(requireContext())
-
-        // Inflate the layout for this fragment
-        var userList = MutableLiveData<MutableList<User>>()
-
-
-        var user = User("1","Konrad","Konrad123", mutableListOf(),
-            Pattern(mutableListOf(Cell(1,0))),Settings())
-
-        var gson = Gson()
-        var jsonString = gson.toJson(user)
-        Log.d("myTag",jsonString)
-
-        val api = Retrofit.Builder().baseUrl(ApiRoutes.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(IApiRequest::class.java)
-
-        //api.addJSon(user)
-
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = api.getJson().awaitResponse()
-            if (response.isSuccessful){
-                val data= response.body()!!
-                // postValue instead of value, because it's called asynchronous
-                userList.postValue(mutableListOf(data))
-                Log.d("api-connection",userList.value!!.toString())
-
-            }
-            else{
-                Log.d("api-connection","response failed")
-                userList.postValue(ArrayList())
-            }
-        }
-
-        statsListAdapter = StatsListAdapter(userList)
+       // statsListAdapter = StatsListAdapter()
 
 
         return inflater.inflate(R.layout.fragment_stats, container, false)
@@ -81,10 +81,10 @@ class StatsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recyclerView.apply{
-            adapter=statsListAdapter
-            layoutManager=viewManager
-        }
+//        recyclerView.apply{
+//            adapter=statsListAdapter
+//            layoutManager=viewManager
+//        }
 
     }
 
