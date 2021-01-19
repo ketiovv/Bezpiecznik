d465bc96b3439d5289d4b3b2da386a363bae8e56
Konrad <konrlub566@student.polsl.pl>
2021-01-19 15:01:31 +0100

    Added onPause metod

--- Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/TestsFragment.kt
+++ Bezpiecznik/app/src/main/java/com/example/bezpiecznik/views/TestsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.bezpiecznik.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,28 +10,8 @@ import androidx.preference.PreferenceManager
 import com.example.bezpiecznik.R
 import kotlinx.android.synthetic.main.fragment_tests.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TestsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TestsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -67,23 +48,29 @@ class TestsFragment : Fragment() {
         pattern_lock_id.removeAllViews()
         pattern_lock_id.initDots()
     }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("myTag", "onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("myTag", "onPause")
+
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("myTag", "onDetach")
+
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TestsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TestsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = TestsFragment()
     }
 }
